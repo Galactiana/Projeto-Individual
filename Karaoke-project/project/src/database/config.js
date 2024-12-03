f6406769db806cbeb33f32a6284b7d6f10165805
Galactiana <ana.barrocal@sptech.school>
2024-12-02 21:41:05 -0300

    removedo error

--- Karaoke-project/project/src/database/config.js
+++ Karaoke-project/project/src/database/config.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const mySqlConfig = {
     host: "localhost",
-    database: "karaokeQuiz",
+    database: "KaraokeQuiz",
     user: "luffy",
     password: "nika"
 };
